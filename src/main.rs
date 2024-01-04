ab9638fb60e8b241d61449ebf80e8f3808aee4c6
zhaopeng <>
2024-01-04 01:49:19 +0100

    use proto message in the publisher and subscriber

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ use chrono::DateTime;
 use chrono::prelude::*;
 use std::sync::{Arc, Mutex};
 use std::thread;
+use uuid::Uuid;
+use protobuf:: Message;
+
+include!(concat!(env!("OUT_DIR"), "/protos/mod.rs"));
+
+use ActionMessages::{ActionRequest, ActionResponse};
 
 /// helper function for debugging variable types
 fn print_type_of<T> (t: &T) {
@@ -22,18 +28,18 @@ fn generate_timestamp()->String {
 
 ///////// Generic message type
 #[derive(Debug)]
-struct Message<T> {
+struct MessageEnvelope<T> {
     data: T,
     timestamp: String,
 }
 
 ///////// Generic Publisher struct
 struct Publisher<T> {
-    messagesList: Arc<Mutex<Vec<Message<T>>>>,
+    messagesList: Arc<Mutex<Vec<MessageEnvelope<T>>>>,
 }
 
 impl<T: std::fmt::Debug> Publisher<T> {
-    fn new(messagesList: Arc<Mutex<Vec<Message<T>>>>) -> Self {
+    fn new(messagesList: Arc<Mutex<Vec<MessageEnvelope<T>>>>) -> Self {
         Publisher {
             messagesList
         }
@@ -44,20 +50,17 @@ impl<T: std::fmt::Debug> Publisher<T> {
         let mut messagesList = self.messagesList.lock().unwrap();
         // print_type_of(&messagesList);
         let timestamp = generate_timestamp();
-        messagesList.push(Message { data, timestamp });
-        /*if messagesList.is_empty() {
-            println!("empty list");
-        }*/
+        messagesList.push(MessageEnvelope { data, timestamp });
     }
 }
 
 ///////// Generic Subscriber struct
 struct Subscriber<T> {
-    messagesList: Arc<Mutex<Vec<Message<T>>>>,
+    messagesList: Arc<Mutex<Vec<MessageEnvelope<T>>>>,
 }
 
 impl<T: Clone + std::fmt::Debug> Subscriber<T> {
-    fn new(messagesList: Arc<Mutex<Vec<Message<T>>>>) -> Self {
+    fn new(messagesList: Arc<Mutex<Vec<MessageEnvelope<T>>>>) -> Self {
         Subscriber { messagesList }
     }
 
@@ -85,9 +88,9 @@ impl<T: Clone + std::fmt::Debug> Subscriber<T> {
 }
 
 ///////////////////////////
-// Main function
+// test and main functions
 ///////////////////////////
-fn main() {
+fn test_pubsub() {
     // shared data container for publishing and reading messages
     let messagesList = Arc::new(Mutex::new(Vec::new()));
 
@@ -109,4 +112,39 @@ fn main() {
 
     // Wait for the subscriber thread to finish
     subscriber_handle.join().unwrap();
+}
+
+fn run_proto_pubsub() {
+    // shared data container for publishing and reading messages
+    let messagesList = Arc::new(Mutex::new(Vec::new()));
+
+    // Create instances of the Publisher and Subscriber with i32 data type
+    let publisher : Publisher<ActionRequest>  = Publisher::new(Arc::clone(&messagesList));
+    let subscriber : Subscriber<ActionRequest> = Subscriber::new(Arc::clone(&messagesList));
+
+    // Spawn the subscriber in a separate thread
+    let subscriber_handle = thread::spawn(move || {
+        subscriber.subscribe();
+    });
+
+    // Publish some data in the main thread
+    for i in 1..=10 {
+        let mut req_msg = ActionRequest::new();
+        let id = Uuid::new_v4();
+        req_msg.guid = id.to_string();
+        req_msg.value = i;
+        req_msg.details.push("aaaa".to_string());
+        req_msg.details.push("zzzz".to_string());
+        println!("Published new message {}", req_msg);
+        
+        publisher.publish(req_msg);
+        sleep_ms(300);
+    }
+
+    // Wait for the subscriber thread to finish
+    subscriber_handle.join().unwrap();
+}
+
+fn main() {
+    run_proto_pubsub();
 }
